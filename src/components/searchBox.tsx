e67b977a214c95737588a590dd700b1254d92b4c
Cathy <cathy.healey@gmail.com>
2022-03-02 21:25:01 -0500

    lesson 10

--- src/components/searchBox.tsx
+++ src/components/searchBox.tsx
@@ -1,17 +1,96 @@
-// import { ChangeEvent } from "react";
-// import { FunctionComponent } from "react";
-// import usePlacesAutocomplete, {
-//   getGeocode,
-//   getLatLng,
-// } from "use-places-autocomplete";
-// import { useGoogleMapsScript, Libraries } from "use-google-maps-script";
-// import {
-//   Combobox,
-//   ComboboxInput,
-//   ComboboxPopover,
-//   ComboboxList,
-//   ComboboxOption,
-// } from "@reach/combobox";
-// import "@reach/combobox/styles.css";
+import { ChangeEvent } from "react";
+import { FunctionComponent } from "react";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import { useGoogleMapsScript, Libraries } from "use-google-maps-script";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+interface ISearchBoxProps {
+  onSelectAddress: (
+    address: string,
+    latitude: number,
+    longitude: number
+  ) => void;
+  defaultValue: string;
+}
+const libraries: Libraries = ["places"];
+export const SearchBox = ({
+  onSelectAddress,
+  defaultValue,
+}: ISearchBoxProps) => {
+  const { isLoaded, loadError } = useGoogleMapsScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? "",
+    libraries,
+  });
+  if (!isLoaded) return null;
+  if (loadError) return <div>Error loading</div>;
 
-export {};
+  return (
+    <ReadySearchBox
+      onSelectAddress={onSelectAddress}
+      defaultValue={defaultValue}
+    />
+  );
+};
+
+function ReadySearchBox({ onSelectAddress, defaultValue }: ISearchBoxProps) {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete({ debounce: 300, defaultValue });
+
+  const handleSelect = async (address: string) => {
+    setValue(address, false);
+    clearSuggestions();
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      //   const onSelectAddress = (
+      //     address: string,
+      //     latitude: number | null,
+      //     longitude: number | null
+      //   ) => void {};
+      onSelectAddress(address, lat, lng);
+    } catch (err) {
+      console.log("ERROR ", err);
+    }
+  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    if (e.target.value === "") {
+      onSelectAddress("", 0, 0);
+    }
+  };
+
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        id="search"
+        value={value}
+        onChange={handleChange}
+        disabled={!ready}
+        placeholder={"Searh your location"}
+        className="w-full p-2"
+        autoComplete="off"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {data.map(({ place_id, description }) => (
+            <ComboboxOption key={place_id} value={description} />
+          ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+}
