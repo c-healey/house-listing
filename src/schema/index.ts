645ee97221a5c16f5739c275f1124cf8022b9005
Cathy <cathy.healey@gmail.com>
2022-03-04 20:22:24 -0500

    added graphql server

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,16 @@
-// import { buildSchemaSync } from "type-graphql";
+import { buildSchemaSync, Resolver, Query } from "type-graphql";
 // import { ImageResolver } from "./image";
 // import { HouseResolver } from "./house";
-// import { authChecker } from "./auth";
-
-export {};
+import { authChecker } from "./auth";
+@Resolver()
+class DummyResolver {
+  @Query((_returns) => String)
+  hello() {
+    return "nice to meet you!";
+  }
+}
+export const schema = buildSchemaSync({
+  resolvers: [DummyResolver],
+  emitSchemaFile: process.env.NODE_ENV === "development",
+  authChecker,
+});
